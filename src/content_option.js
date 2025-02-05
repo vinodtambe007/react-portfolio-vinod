24f2f6c70e8974ef2d1113e42089ef14bbcac79d
vinod <vinodtambe070@gmail.com>
2025-02-05 11:59:41 +0530

    portfolio

---
+++ src/content_option.js
@@ -0,0 +1,121 @@
+const logotext = "Vinod";
+const meta = {
+    title: "Vinod Tambe",
+    description: "I’m Vinod Tambe Full Stack Developer ,currently working ",
+};
+
+const introdata = {
+    title: "I’m Vinod Tambe",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop backend Api",
+    },
+    description: "Full Stack Developer with expertise in React.js, Node.js, Express, and MongoDB.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "Passionate Full Stack Developer with a strong focus on ReactJS and NodeJS. With a deep understanding of both front-end and back-end technologies, I thrive in building dynamic, responsive, and scalable web applications",
+};
+const worktimeline = [{
+        jobtitle: "Web Developer",
+        where: "Nashik, Maharashtra",
+        date: "Dec 2023 - May 2024",
+    },
+    {
+        jobtitle: "Associate Solution Engineer",
+        where: "Pune, Nashik",
+        date: "May 2024 - Present",
+    }
+];
+
+const skills = [{
+        name: "ReactJs",
+        value: 90,
+    },
+    {
+        name: "NodeJs",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "MongoDB",
+        value: 90,
+    },
+    {
+        name: "Html5",
+        value: 100,
+    },
+    {
+        name: "css",
+        value: 100,
+    },
+    {
+        name: "TailwindCss",
+        value: 100,
+    },
+    {
+        name: "ExpressJs",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "Frontend Web Development with React.js",
+        description: " I specialize in building fast, responsive, and interactive web applications using ReactJS. By utilizing modern web development practices, I create dynamic user interfaces that deliver seamless user experiences across various devices. From single-page applications (SPAs) to complex web platforms, I focus on scalability, performance, and usability.",
+    },
+    {
+        title: "Backend Development with Node.js & Express.js",
+        description: "As a backend developer, I specialize in building robust, scalable, and efficient server-side solutions using Node.js and Express.js. I focus on creating RESTful APIs, managing databases, handling authentication, and ensuring smooth server-side logic. With experience in designing scalable back-end architectures, I deliver high-performing, secure, and reliable web services. Whether you need API development, database integration, or server-side optimization, I can handle it all with precision and performance."
+    },
+    {
+        title: "Full Stack Web Development",
+        description: " As a full stack developer, I bring both front-end and back-end expertise to create complete, scalable web solutions. With proficiency in ReactJS for the front-end and NodeJS for the back-end, I deliver end-to-end solutions that include database management, server-side logic, and API integrations. Whether you need a custom web app or a comprehensive web solution, I can handle all aspects of development.",
+    }
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Service-App (Urban Clap Clone).",
+        link: "https://github.com/vinodtambe007/Service-App",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Online-Mcq-Test-App",
+        link: "https://github.com/vinodtambe007/Online-Mcq-Test-App",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Job-Portal.",
+        link: "https://github.com/vinodtambe007/Job-Portal",
+    }
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vinodtambe070@gmail.com",
+    YOUR_FONE: "+91-7387816465",
+    description: "I’d love to hear from you! Whether you have a question, want to collaborate, or discuss a potential project, feel free to reach out. I'm always open to new opportunities and would be happy to connect and assist with any web development needs you may have.",
+};
+
+const socialprofils = {
+    github: "https://github.com/vinodtambe007",
+    linkedin: "https://www.linkedin.com/in/vinod-tambe/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
